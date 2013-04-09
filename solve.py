909a8a59fa539b9867fef709dc541715dc0dca39
Mark Langen <stravant@ubuntu.(none)>
2013-04-08 21:27:42 -0700

    Working on solver

---
+++ solve.py
@@ -0,0 +1,105 @@
+
+from solver import *
+
+def slove_system(m, input_i, output_i, state_i, nstate_i, input_n, output_n, state_n):
+	"""
+	Solve a system with data gathered in the matrix m, and the input / output
+	/ state names / indicies in that matrix specified. 
+	"""
+	# total cols in the matrix to solve
+	ncols = len(input_i)+len(output_i)+len(state_i)
+
+	# the new matrix to populate
+	new_m = Matrix(ncols, m.get_num_rows(), 0)
+
+	# output names
+	output_names_by_index = []
+	for in_i in input_i:
+
+	# transcribe the rows of the data matrix into the new matrix
+	for row_i in range(m.get_num_rows()):
+		row = m.get_row(row_i)
+		newrow = []
+
+		# row format:
+		# [ input | curstate | output | nextstate JK ]
+
+		# first, the inputs
+		for in_i in input_i:
+			newrow.append(row[in_i])
+
+		# then the current state
+		for st_i in state_i:
+			newrow.append(row[st_i])
+
+		# then the outputs
+		for out_i in output_i:
+			newrow.append(row[out_i])
+
+		# then the next state J and K values
+		for ns_i in nstate_i:
+			a = row[state_i[ns_i]]
+			b = row[nstate_i[ns_i]]
+			if a == 0 and b == 1:
+				# 0->1, JK = 1X
+				newrow.append(1)
+				newrow.append(EITHER)
+			elif a == 0 and b == 0:
+				# 0->0, JK = 0X
+				newrow.append(0)
+				newrow.append(EITHER)
+			elif a == 1 and b == 0:
+				# 1->0, JK = X1
+				newrow.append(EITHER)
+				newrow.append(1)
+			else:
+				# 1->1, JK = X0
+				newrow.append(EITHER)
+				newrow.append(0)
+
+		# and the next state K values
+		for ns_i in nstate_i:
+
+
+	# now, we need to gather the results for each output in terms of a set of
+	# input and current state.
+	result_list = [] # [ (name,simplified expression) ]
+
+	# get arguments for simplifier
+	all_input_index_list = []
+	all_input_name_list = []
+	# add inputs and states
+	for i in range(len(input_i)):
+		all_input_name_list.append(input_n[i])
+		all_input_index_list.append(i)
+	for i in range(len(state_i)):
+		all_input_name_list.append(state_n[i])
+		all_input_index_list.append(len(input_i) + i)
+
+	# first get the outputs in terms of the inputs
+	for out_i in range(len(output_i)):
+		implicant_list = solve_and_or(new_m, 
+		                              len(input_i)+len(state_i)+out_i,
+		                              all_input_index_list)
+		result = imp_list_to_string(implicant_list, all_input_name_list)
+		result_list.append((output_n[out_i], result))
+
+	# then get the next-state JK inputs
+	for st_i in range(len(state_i)):
+		# J input
+		implicant_list = solve_and_or(new_m,
+		                              len(input_i)+len(state_i)+len(output_i)\
+		                              +2*st_i,
+		                              all_input_index_list)
+		result = imp_list_to_string(implicant_list, all_input_name_list)
+		result_list.append((state_n[st_i]+'_J', result))
+
+		# K input
+		implicant_list = solve_and_or(new_m,
+		                              len(input_i)+len(state_i)+len(output_i)\
+		                              +2*st_i+1)
+		result = imp_list_to_string(implicant_list, all_input_name_list)
+		result_list.append((state_n[st_i]+'_K', result))
+
+	# and return that result list
+	return result_list
--- solver.py
+++ solver.py
@@ -239,6 +239,8 @@ def imp_to_string(imp, var_names):
 			s += var_names[i]+"'"
 		elif v == ONE:
 			s += var_names[i]
+	if len(s) == 0:
+		s = "1"
 	return s
 
 
@@ -249,30 +251,30 @@ def imp_list_to_string(imp_list, var_names):
 	return ' + '.join([imp_to_string(imp, var_names) for imp in imp_list])
 
 
-def test():
-	# inputs
-	func = lambda a,b,c,d: a or not c
-	varnames = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-	nvars = 4
+# def test():
+# 	# inputs
+# 	func = lambda a,b,c,d: a or not c
+# 	varnames = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+# 	nvars = 4
 
-	# build table
-	m = Matrix(2**nvars, nvars+1, 0)
-	for row in range(2**nvars):
-		args = []
-		n = row
-		for i in range(nvars):
-			args.append(n%2 == 1)
-			n >>= 1
-		m.bin_set_row(row, row, nvars)
-		m.set_val(row, nvars, 1 if func(*(args[::-1])) else 0)
+# 	# build table
+# 	m = Matrix(2**nvars, nvars+1, 0)
+# 	for row in range(2**nvars):
+# 		args = []
+# 		n = row
+# 		for i in range(nvars):
+# 			args.append(n%2 == 1)
+# 			n >>= 1
+# 		m.bin_set_row(row, row, nvars)
+# 		m.set_val(row, nvars, 1 if func(*(args[::-1])) else 0)
 
-	#print(m)
+# 	#print(m)
 
-	res = solve_and_or(m, nvars, [x for x in range(nvars)])
-	#print("=> Done: ")
-	print(imp_list_to_string(res, varnames))
+# 	res = solve_and_or(m, nvars, [x for x in range(nvars)])
+# 	#print("=> Done: ")
+# 	print(imp_list_to_string(res, varnames))
 
-test()
+# test()
 
 
 
